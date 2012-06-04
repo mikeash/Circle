ccbbcf822c248da384e6b45c83eb3f90742d2fed
Michael Ash <mikeash@MikeAir.local>
2012-06-03 21:19:43 -0400

    add some real actual unit tests

--- CircleTests/CircleTests.m
+++ CircleTests/CircleTests.m
@@ -8,25 +8,136 @@
 
 #import "CircleTests.h"
 
+#import "CircleSimpleCycleFinder.h"
+
+
+@interface Referrer : NSObject
+@property (strong) id ptr1;
+@property (strong) id ptr2;
+@end
+@implementation Referrer
+@end
+
 @implementation CircleTests
 
-- (void)setUp
+- (void)testEmptyCollector
 {
-    [super setUp];
-    
-    // Set-up code here.
+    CircleSimpleCycleFinder *collector = [[CircleSimpleCycleFinder alloc] init];
+    [collector collect];
 }
 
-- (void)tearDown
+- (void)testSimpleCycle
 {
-    // Tear-down code here.
+    CircleSimpleCycleFinder *collector = [[CircleSimpleCycleFinder alloc] init];
     
-    [super tearDown];
+    __weak id weakObj;
+    @autoreleasepool {
+        Referrer *a = [[Referrer alloc] init];
+        Referrer *b = [[Referrer alloc] init];
+        [a setPtr1: b];
+        [b setPtr1: a];
+        weakObj = a;
+        
+        [collector addCandidate: a];
+    }
+    
+    @autoreleasepool {
+        STAssertNotNil(weakObj, @"Weak pointer to cycle should not be nil before running the collector");
+    }
+    [collector collect];
+    STAssertNil(weakObj, @"Collector didn't collect a cycle");
 }
 
-- (void)testExample
+- (void)testSingleObjectCycle
 {
-    STFail(@"Unit tests are not implemented yet in CircleTests");
+    CircleSimpleCycleFinder *collector = [[CircleSimpleCycleFinder alloc] init];
+    
+    __weak id weakObj;
+    @autoreleasepool {
+        Referrer *a = [[Referrer alloc] init];
+        [a setPtr1: a];
+        weakObj = a;
+        
+        [collector addCandidate: a];
+    }
+    
+    @autoreleasepool {
+        STAssertNotNil(weakObj, @"Weak pointer to cycle should not be nil before running the collector");
+    }
+    [collector collect];
+    STAssertNil(weakObj, @"Collector didn't collect a cycle");
+}
+
+- (void)testBlockCycle
+{
+    CircleSimpleCycleFinder *collector = [[CircleSimpleCycleFinder alloc] init];
+    
+    __weak id weakObj;
+    @autoreleasepool {
+        Referrer *a = [[Referrer alloc] init];
+        id b = [^{ NSLog(@"%p", a); } copy];
+        [a setPtr1: b];
+        weakObj = a;
+        
+        [collector addCandidate: a];
+    }
+    
+    @autoreleasepool {
+        STAssertNotNil(weakObj, @"Weak pointer to cycle should not be nil before running the collector");
+    }
+    [collector collect];
+    STAssertNil(weakObj, @"Collector didn't collect a cycle");
+}
+
+- (void)testComplexCycle
+{
+    CircleSimpleCycleFinder *collector = [[CircleSimpleCycleFinder alloc] init];
+    
+    __weak id weakObj;
+    @autoreleasepool {
+        Referrer *a = [[Referrer alloc] init];
+        Referrer *b = [[Referrer alloc] init];
+        Referrer *c = [[Referrer alloc] init];
+        [a setPtr1: b];
+        [a setPtr2: c];
+        [b setPtr1: a];
+        [b setPtr2: c];
+        [c setPtr1: a];
+        [c setPtr2: b];
+        weakObj = a;
+        
+        [collector addCandidate: a];
+    }
+    
+    @autoreleasepool {
+        STAssertNotNil(weakObj, @"Weak pointer to cycle should not be nil before running the collector");
+    }
+    [collector collect];
+    STAssertNil(weakObj, @"Collector didn't collect a cycle");
+}
+
+- (void)testReachableCycle
+{
+    CircleSimpleCycleFinder *collector = [[CircleSimpleCycleFinder alloc] init];
+    
+    __weak id weakObj;
+    id obj;
+    @autoreleasepool {
+        Referrer *a = [[Referrer alloc] init];
+        Referrer *b = [[Referrer alloc] init];
+        [a setPtr1: b];
+        [b setPtr1: a];
+        weakObj = a;
+        obj = b;
+        
+        [collector addCandidate: a];
+    }
+    
+    @autoreleasepool {
+        STAssertNotNil(weakObj, @"Weak pointer to cycle should not be nil before running the collector");
+    }
+    [collector collect];
+    STAssertNotNil(weakObj, @"Collector collected a referenced cycle");
 }
 
 @end
